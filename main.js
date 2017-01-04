49740db551ef551b0b331e9302baf0c1e92ae9c1
marcusthorstrom <marcus@thorstrom.se>
2017-01-04 16:06:04 +0100

    Added fault checks

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ app.listen(3000, function () {
 })
 
 app.post("/:chanel/:number/:power", function (req, res) {
+
   var chanel = req.params.chanel;
   var number = req.params.number;
   var power = req.params.power;
@@ -18,9 +19,23 @@ app.post("/:chanel/:number/:power", function (req, res) {
   console.log("number: ", number);
   console.log("power: ", power);
 
+  if (chanel != ("A" || "B" || "C" || "D" )) {
+    res.status(400).send("Wrong chanel");
+    return;
+  }
+  if (number != ("1" || "2" || "3" || "4")) {
+    res.status(400).send("Wrong number");
+    return;
+  }
+  power = power.charAt(0).toUpperCase() + power.subStr(1).toLowerCase();
+  if(power != ("On" || "Off")) {
+    res.status(400).send("Wrong power");
+    return;
+  }
+
   child_process.exec("python test.py 5 "+ chanel +" "+number+" "+power, function(error, stdout, stderr){
       if(!error) {
-        res.status(200);
+        res.status(200).send("it's "+power+"!");
       } else {
         console.log(error);
       }
